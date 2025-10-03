74a6d904f1b3d327ff70958a4462049fd3bee052
unknown <nikolajanembo@gmail.com>
2025-10-03 17:57:21 +0200

    Added player borders

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,23 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UIElements;
 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float speed = 5f;
-    public ProjectileController laserPrefab; 
+
+    public ProjectileController laserPrefab;
+
+    private bool canMoveLeft = true;
+    private bool canMoveRight = true;
  
     void Update()
     {
-        if (Input.GetKey(KeyCode.A))
+        if (Input.GetKey(KeyCode.A) && canMoveLeft == true)
         {
             this.transform.position += Vector3.left * speed * Time.deltaTime;
         }
-        if (Input.GetKey(KeyCode.D))
+        if (Input.GetKey(KeyCode.D) && canMoveRight == true)
         {
             this.transform.position += Vector3.right * speed * Time.deltaTime;
         }
@@ -21,9 +26,33 @@ public class PlayerController : MonoBehaviour
             Shoot();            
         }
     }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "BorderRight")
+        {
+            canMoveRight = false;
+        }
+        if (collision.gameObject.name == "BorderLeft")
+        {
+            canMoveLeft = false; 
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "BorderRight")
+        {
+            canMoveRight = true;
+        }
+        if (collision.gameObject.name == "BorderLeft")
+        {
+            canMoveLeft = true;
+        }
+
+    }
     private void Shoot()
     {
         Instantiate(this.laserPrefab, this.transform.position, Quaternion.identity);
     }
+    
 
 }
